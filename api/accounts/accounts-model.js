fed3f9f11f758fd22a9839346cc79c72dbadef34
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-18 14:06:35 -0800

    completed all model functions

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,31 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config');
+
+const getAll = async () => {
+  const result = db('accounts')
+  return result
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+const getById = async (id) => {
+  const result = db('accounts').where({id}).first()
+  return result
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async (account) => {
+  const [resultId] = await db('accounts').insert(account)
+  const result = getById(resultId)
+  return result
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  await db('accounts').where({id}).update(account)
+  const result = getById(id)
+  return result
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async (id) => {
+  const toBeDeleted = getById(id)
+  await db('accounts').where({id}).del()
+  return toBeDeleted
 }
 
 module.exports = {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,29 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const Accounts = require('./accounts-model');
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  const accounts = await Accounts.getAll();
+  console.log(accounts);
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', async (req, res, next) => {
+  const account = await Accounts.getById(req.params.id);
+  console.log(account);
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', async (req, res, next) => {
+  const newAccount = await Accounts.create(req.body);
+  console.log(newAccount);
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', async (req, res, next) => {
+  const updatedAccount = await Accounts.updateById(req.params.id, req.body);
+  console.log(updatedAccount);
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', async (req, res, next) => {
+  const deletedAccount = await Accounts.deleteById(req.params.id);
+  console.log(deletedAccount);
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
