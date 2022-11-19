79855306afe9814f95f1cab682aaaeda59a1a714
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-18 16:32:31 -0800

    created router endpoints

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -7,13 +7,21 @@ const {
 } = require('./accounts-middleware');
 
 router.get('/', async (req, res, next) => {
-  const accounts = await Accounts.getAll();
-  console.log(accounts);
+  try {
+    const accounts = await Accounts.getAll();
+    res.status(200).json(accounts);
+  } catch(err) {
+    next(err);
+  }
 })
 
 router.get('/:id', checkAccountId, async (req, res, next) => {
-  const account = await Accounts.getById(req.params.id);
-  console.log(account);
+  try {
+    const account = await Accounts.getById(req.params.id);
+    res.status(200).json(account);
+  } catch(err) {
+    next(err);
+  }
 })
 
 router.post(
@@ -21,8 +29,12 @@ router.post(
   checkAccountPayload, 
   checkAccountNameUnique, 
   async (req, res, next) => {
-    const newAccount = await Accounts.create(req.body);
-    console.log(newAccount);
+    try {
+      const newAccount = await Accounts.create(req.body);
+      res.status(201).json(newAccount);
+    } catch(err) {
+      next(err);
+    }
   }
 );
 
@@ -32,14 +44,22 @@ router.put(
   checkAccountId, 
   checkAccountNameUnique, 
   async (req, res, next) => {
-    const updatedAccount = await Accounts.updateById(req.params.id, req.body);
-    console.log(updatedAccount);
+    try {
+      const updatedAccount = await Accounts.updateById(req.params.id, req.body);
+      res.status(200).json(updatedAccount);
+    } catch(err) {
+      next(err);
+    }
   }
 );
 
 router.delete('/:id', checkAccountId, async (req, res, next) => {
-  const deletedAccount = await Accounts.deleteById(req.params.id);
-  console.log(deletedAccount);
+  try {
+    const deletedAccount = await Accounts.deleteById(req.params.id);
+    res.status(200).json(deletedAccount);
+  } catch(err) {
+    next(err);
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
