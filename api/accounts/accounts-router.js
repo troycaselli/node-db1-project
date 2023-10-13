0a7844da5b12cea551119414114352e6383d7435
troycaselli <swordandtrowel1689@gmail.com>
2023-10-13 10:04:41 -0700

    fixed eslint warning

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,64 +1,69 @@
-const router = require('express').Router();
-const Accounts = require('./accounts-model');
+const router = require("express").Router();
+const Accounts = require("./accounts-model");
 const {
   checkAccountPayload,
   checkAccountNameUnique,
-  checkAccountId
-} = require('./accounts-middleware');
+  checkAccountId,
+} = require("./accounts-middleware");
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const accounts = await Accounts.getAll();
     res.status(200).json(accounts);
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
-})
+});
 
-router.get('/:id', checkAccountId, async (req, res, next) => {
+/* eslint-disable */
+router.get("/:id", checkAccountId, async (req, res, next) => {
+  /* eslint-disable */
   res.status(200).json(req.account);
-})
+});
 
 router.post(
-  '/', 
-  checkAccountPayload, 
-  checkAccountNameUnique, 
+  "/",
+  checkAccountPayload,
+  checkAccountNameUnique,
   async (req, res, next) => {
     try {
       const newAccount = await Accounts.create(req.body);
       res.status(201).json(newAccount);
-    } catch(err) {
+    } catch (err) {
       next(err);
     }
   }
 );
 
 router.put(
-  '/:id', 
-  checkAccountPayload, 
-  checkAccountId, 
-  checkAccountNameUnique, 
+  "/:id",
+  checkAccountPayload,
+  checkAccountId,
+  checkAccountNameUnique,
   async (req, res, next) => {
     try {
       const updatedAccount = await Accounts.updateById(req.params.id, req.body);
       res.status(200).json(updatedAccount);
-    } catch(err) {
+    } catch (err) {
       next(err);
     }
   }
 );
 
-router.delete('/:id', checkAccountId, async (req, res, next) => {
+router.delete("/:id", checkAccountId, async (req, res, next) => {
   try {
     await Accounts.deleteById(req.params.id);
     res.status(200).json(req.account);
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  res.status(err.status || 500).json({message: err.message || 'Something went terribly wrong'});
-})
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Something went terribly wrong" });
+});
 
 module.exports = router;
